09ae641ecd52f6115ec6bb71ff22822ed5225867
Sayam <sayamalvi07@gmail.com>
2022-08-22 17:25:30 +0530

    Design complete

---
+++ index.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('.btn');
+const cardTwo = document.querySelector('.card_state-two');
+const cardOne = document.querySelector('.card_state-two');
+
+btn.addEventListener('click', () => {
+  })
