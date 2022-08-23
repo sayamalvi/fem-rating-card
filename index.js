c593c33ce08c625f6b77c5826f001e27cb50b36d
Sayam <sayamalvi07@gmail.com>
2022-08-23 15:22:55 +0530

    Challenge complete

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const btn = document.querySelector('.btn');
 const cardTwo = document.querySelector('.card_state-two');
-const cardOne = document.querySelector('.card_state-two');
+const cardOne = document.querySelector('.card_state-one');
+const numbers = document.querySelectorAll('.number');
+const rate = document.querySelector('.rate');
 
 btn.addEventListener('click', () => {
+  cardOne.classList.add('hidden');
+  cardTwo.classList.remove('hidden')
+})
+
+numbers.forEach((number) => {
+  number.addEventListener('click', () => {
+    const value = number.textContent;
+    rate.textContent = value;
   })
+})
+
